6bbd343b8c3999200a6bb1617ba9ae0bea7f4bf4
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2016-08-24 13:13:48 -0400

    css

--- js/main.js
+++ js/main.js
@@ -16,26 +16,26 @@
 
 	// OffCanvass
 	var offCanvass = function() {
-		$('body').on('click', '.js-fh5co-menu-btn, .js-fh5co-offcanvass-close', function(){
-			$('#fh5co-offcanvass').toggleClass('fh5co-awake');
+		$('body').on('click', '.js-menu-btn, .js-offcanvass-close', function(){
+			$('#offcanvass').toggleClass('awake');
 		});
 	};
 
 	// Click outside of offcanvass
 	var mobileMenuOutsideClick = function() {
 		$(document).click(function (e) {
-	    var container = $("#fh5co-offcanvass, .js-fh5co-menu-btn");
+	    var container = $("#offcanvass, .js-menu-btn");
 	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-	    	if ( $('#fh5co-offcanvass').hasClass('fh5co-awake') ) {
-	    		$('#fh5co-offcanvass').removeClass('fh5co-awake');
+	    	if ( $('#offcanvass').hasClass('awake') ) {
+	    		$('#offcanvass').removeClass('awake');
 	    	}
 	    }
 		});
 
 		$(window).scroll(function(){
 			if ( $(window).scrollTop() > 500 ) {
-				if ( $('#fh5co-offcanvass').hasClass('fh5co-awake') ) {
-		    		$('#fh5co-offcanvass').removeClass('fh5co-awake');
+				if ( $('#offcanvass').hasClass('awake') ) {
+		    		$('#offcanvass').removeClass('awake');
 		    	}
 	    	}
 		});
